fc4584fcb4cad5c09620ed4345cc8d30f3645a66
Amy Beeston <amybeeston93@gmail.com>
2015-06-04 17:01:16 -0600

    Finished step 7

--- app/src/main/java/com/example/amy/multithread/MultiThread.java
+++ app/src/main/java/com/example/amy/multithread/MultiThread.java
@@ -1,18 +1,16 @@
 package com.example.amy.multithread;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,6 +21,7 @@ public class MultiThread extends ActionBarActivity {
 
     private String fileName = "numbers.txt";
     private List<String> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,50 +51,65 @@ public class MultiThread extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void create() {
-        File file = new File(this.getFilesDir(), fileName);
+    public void create(View view) throws IOException {
         try {
-            FileOutputStream ostream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            // open the file
+            File file = new File(this.getFilesDir(), fileName);
 
-            // write contents
-            for (int i = 1; i < 11; i++) {
-                String content = i + "\n";
-                ostream.write(content.getBytes());
-                Thread.sleep(250);
+            if (file.createNewFile()) {
+                FileWriter fileWriter = new FileWriter(file);
+
+                // write contents
+                for (int i = 1; i < 11; i++) {
+                    String content = i + "\n";
+                    fileWriter.write(content);
+                    //Thread.sleep(250);
+                }
+                fileWriter.close();
             }
-            ostream.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-
-
     }
 
-    public void load() {
-        ListView view = (ListView)findViewById(R.id.listView);
+    public void load(View view) {
         try {
             FileReader fReader = new FileReader(new File(this.getFilesDir(), fileName));
-            BufferedReader br = new BufferedReader(fReader);
+            boolean cont = true;
+            while (cont) {
+                if (fReader.ready()) {
+                    BufferedReader br = new BufferedReader(fReader);
 
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                System.out.println(line);
-
-                list.add(line);
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        list.add(line);
+                        //Thread.sleep(250);
+                    }
+                    br.close();
+                    cont = false;
+                }
             }
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-            view.setAdapter(adapter);
+            ListView lView = (ListView)findViewById(R.id.listView);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+            lView.setAdapter(adapter);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void clear() {
+    public void clear(View view) {
+        // delete the file
+        File file = new File(this.getFilesDir(), fileName);
+        file.delete();
+
+        // clear the list
+        list.clear();
+        ListView lView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        //adapter.addAll(list);
+        lView.setAdapter(adapter);
     }
 }
